43d03b3eb4eaf78165566770183b97b386db7676
albertysenna <albertysenna@gmail.com>
2026-03-23 23:54:14 -0300

    Corrigindo erros

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -60,7 +60,7 @@ public class LoginPage {
 
     public void verificaLoginSucesso(){
 
-        checkMessage(By.id("swal2-title"),"Login realizado xxx");
+        checkMessage(By.id("swal2-title"),"Login realizado");
 
 //        String textoLoginSucesso = DriverFactory.getDriver().findElement(By.id("swal2-title")).getText();
 //        Assertions.assertEquals( "Login realizado", textoLoginSucesso, "Os textos não são iguais!");
